559c9eaffc379f85c717cf0c26b7548acaa1f94e
Abiodun Samuel <abiodunsamyemi@gmail.com>
2021-11-04 12:51:17 +0100

    4-11-21

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -5,7 +5,7 @@
         <div class="container">
             <div class="row d-flex justify-content-center">
                 <div class="col-lg-6">
-                    <img src="{{ asset('images/backgrounds/blog-bg.jpg') }}" alt="">
+                    <img class="img-fluid" src="{{ asset('images/backgrounds/blog-bg.jpg') }}" alt="">
                 </div>
             </div>
         </div>

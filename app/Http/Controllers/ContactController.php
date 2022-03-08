4e3e518d1e44a214b2b6a0a1fd441c0a2ea4dca1
Abiodun Samuel <abiodunsamyemi@gmail.com>
2022-03-08 23:46:43 +0100

    edit google recaptcha

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -16,7 +16,7 @@ class ContactController extends Controller
             'email' => "required|email:rfc,dns,filter",
             'subject' => "required|string|max:255",
             'message' => "required|string|max:255",
-            'g-recaptcha-response' => 'required|recaptchav3:contactus,0.5'
+            'g-recaptcha-response' => 'recaptchav3:contactus,0.5'
         ]);
         Mail::to('contact@abiodunsamuel.com')->send(new ContactMail($mail));
         return back()->with('status', "Your messages has been sent successfully");
